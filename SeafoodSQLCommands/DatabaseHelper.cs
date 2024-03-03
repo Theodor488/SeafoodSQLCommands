c4b51e5da12861edf0a952747fc32894e6d28bd7
Theodor Suciu <theodorsuciu@yahoo.com>
2024-03-02 16:21:00 -0800

    Add project files.

---
+++ SeafoodSQLCommands/DatabaseHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeafoodSQLCommands
+{
+    public class DatabaseHelper
+    {
+        public void ConnectToDatabase(SqlConnection connection, string query)
+        {
+            connection.Open();
+            ExecuteQuery(query, connection);
+        }
+
+        public List<string> ExecuteQuery(string query, SqlConnection connection) 
+        {
+            List<string> results = new List<string>();
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            string data = reader.GetString(0);
+                            results.Add(data);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Found. Reader was empty.");
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public void DisconnectFromDatabase(SqlConnection connection)
+        {
+            connection.Dispose();
+        }
+    }
+}
---
+++ SeafoodSQLCommands/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SeafoodSQLCommands
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=THEO-COMPUTER\\SQLEXPRESS;Initial Catalog=SeafoodDB;Integrated Security=True";
+
+            SeafoodSpeciesManager seafoodSpeciesManager = new SeafoodSpeciesManager();
+            DatabaseHelper databaseHelper = new DatabaseHelper();
+
+            try
+            {
+                // SQL query to retrieve species names containing the text "fish"
+                string query = seafoodSpeciesManager.GetAllSpecies();
+
+                // Create and open connection
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    databaseHelper.ConnectToDatabase(connection, query);
+                }
+                    
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
